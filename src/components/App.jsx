4b63ffe76d3e96fbc8b11dbb7bf25d6a3317447f
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-10-09 18:38:15 +0300

    add redirecting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,23 +9,42 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operation';
 import { selectIsRefreshing } from 'redux/auth/selectors';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const App = () => {
-  const dispatch = useDispatch()
-  const isRefreshing = useSelector(selectIsRefreshing)
-  
-  useEffect(() => {
-    dispatch(refreshUser())
-  }, [dispatch])
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
-  return (
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <div>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>} />
-          <Route path="register" element={<Register/>} />
-          <Route path="login" element={<Login/>} />
-          <Route path="contacts" element={<Contacts/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Register />}
+              />
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+          <Route path="contacts" element={
+          <PrivateRoute redirectTo="/login" component={<Contacts />} /> 
+          } />
           <Route path="*" element={<div>Not Found</div>} />
         </Route>
       </Routes>

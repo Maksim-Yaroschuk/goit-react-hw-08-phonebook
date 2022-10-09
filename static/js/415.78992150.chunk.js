538c9fa51bbbfe7ee5ae25d3c1867c6b448b9c52
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-10-09 18:51:48 +0000

    Deploying to gh-pages from @ Maksim-Yaroschuk/goit-react-hw-08-phonebook@65d880cff8632e017063691fe5738d8588f3b7cb 🚀

---
+++ static/js/415.78992150.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(8346),c=o(4270),h=o(3329);function r(){return(0,h.jsxs)("main",{children:[(0,h.jsx)(c.q,{children:(0,h.jsx)("title",{children:"Phonebook"})}),(0,h.jsx)(t.v0,{children:"Contacts home page"})]})}}}]);
+//# sourceMappingURL=415.78992150.chunk.js.map
